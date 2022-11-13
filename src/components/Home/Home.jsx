daa8e6bf7df6366ac337fba42025c15e0af03de2
tomilin <tomilin229@gmail.com>
2022-11-13 12:46:54 +0500

    changes

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Home.css';
+
+
+const Home = () => {
+    return (
+        <div className={'home'}>
+            Добро пожаловать в webapp admin
+        </div>
+    );
+};
+
+export default Home;
